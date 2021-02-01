a24dd83303d3a3fb964e166d20bc55819cebdd55
dragandobrijevic <dragan16m@gmail.com>
2021-02-01 23:53:22 +0100

    soa label fix

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-bind:class="[class1]">
     <div v-for="(option, index) in options" :key="index">
-      <label v-show="ifLabel" :for="name">{{ label }}:</label>
+      <label v-show="labelX" :for="name">{{ label }}:</label>
       <select class="custom-select mr-sm-2" v-model="selected" @change="updateSoF" required>
         <option disabled value="">Please select one</option>
         <option class="form-check" v-for="(value, key, index) in option" :key="index" :value="key">
@@ -27,12 +27,21 @@ export default {
       options: this.object1.options,
       col: this.object1.col,
       class1: "",
-      selected: this.object1.value,
-      ifLabel: false,
+      selected: this.object1.value
     }
   },
   created() {
     this.class1 = `col col-lg-${this.col}`
+  },
+  computed: {
+    labelX: function () {
+      let ifLabel = false
+      if(this.label==="Source of attention") {
+        ifLabel = true
+        return ifLabel
+      }
+      return ifLabel
+    }
   },
   methods: {
     updateSoF: function () {
